54fd93b4d46ca94cc0473836aeec09d3d0016c3f
supamoe25@gmail.com <supamoe25@gmail.com>
2016-07-27 21:02:08 +0200

    minor fixes - added emerald - gui changes

--- esp8266-iot-ws2812b.ino
+++ esp8266-iot-ws2812b.ino
@@ -18,6 +18,11 @@ bool faders = false;
 const char *ssid = "ESP-net";
 const char *password = "35p-r0cks-iot";
 
+// Update these with values suitable for your network.
+IPAddress ip(192,168,2,242);  //Node static IP
+IPAddress gateway(192,168,2,112);
+IPAddress subnet(255,255,255,0);
+
 MDNSResponder mdns;
 ESP8266WebServer server ( 80 );
 const int led = 13;
@@ -25,25 +30,26 @@ void handleRoot() {
     digitalWrite ( led, 1 );
   String out = "<html><head><title>OcknigmaLabs Wifi light</title></head>";
   out += "<body style='background-color:#323232'>";
-  out += "<span style='display:block; width:100%; font-size:1.5em; font-family:Verdana; color:white; text-align:center'>OcknigmaLabs Wifi light</span><br/>";
+  out += "<span style='display:block; width:100%; font-size:1.5em; font-family:Verdana; color:white; text-align:center'>-# OcknigmaLabs Wifi light  --#--  IP:192.168.2.242 - Node:1 #-</span><br/>";
   out += "<div id='colors' style='padding: 0% 0%; display:block; margin:auto; position:relative;'>";
-  out += "<a href='white'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:white; width:21%; height:4em;'>WHITE</span></a>";
+  out += "<a href='blue'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:blue; width:21%; height:4em;'>BLUE</span></a>";
   out += "<a href='red'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:red; width:21%; height:4em;'>RED</span></a>";
   out += "<a href='green'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:green; width:21%; height:4em;'>GREEN</span></a><br/>";
   out += "</div>";
   out += "<div id='colors' style='padding: 1% 0%; display:block; margin:auto; position:relative;'>";
-  out += "<a href='blue'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:blue; width:21%; height:4em;'>BLUE</span></a>";
-  out += "<a href='pink'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:#f100f1; width:21%; height:4em;'>PINK</span></a>";   
+  out += "<a href='emerald'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:#00ffff; width:21%; height:4em;'>EMERALD</span></a>";
+  out += "<a href='pink'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:#ff00ff; width:21%; height:4em;'>PINK</span></a>";   
   out += "<a href='yellow'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:yellow; width:21%; height:4em;'>YELLOW</span></a><br/>";
   out += "</div>";  
   out += "<div id='effect' style='padding: 0% 0%; display:block; margin:auto; position:relative;'>";
   out += "<a href='looper'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:#f1f1f1; width:21%; height:4em;'>ColorLoop</span></a>";
-  out += "<a href='chaser'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:#e9e9e9; width:21%; height:4em;'>ColorChaser</span></a>";
+  out += "<a href='chaser'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:#d9d9d9; width:21%; height:4em;'>ColorChaser</span></a>";
   out += "<a href='rainbow'><span style='display:inline-table; padding: 1% 6%; margin:auto; text-align:center; background-color:#f1f1f1; width:21%; height:4em;'>RAINBOW</span></a>";
   out += "</div>";  
   out += "<div id='off' style='padding: 1% 0%; display:block; margin:auto; position:relative;'>";
-  out += "<a href='fader'><span style='display:inline-table; padding: 2% 6%; text-align:center; color:black; background-color:white; width:87%; height:4em;'>FADER</span></a><br/>";
-  out += "<a href='black'><span style='display:inline-table; padding: 2% 6%; text-align:center; color:white; background-color:black; width:87%; height:4em;'>OFF / FADER OFF</span></a><br/>";
+  out += "<a href='fader'><span style='display:inline-table; padding: 2% 6%; text-align:center; color:black; background-color:#d9d9d9; width:87%; height:3em;'>FADER</span></a><br/>";
+  out += "<a href='white'><span style='display:inline-table; padding: 2% 6%; text-align:center; color:black; background-color:white; width:38%; height:3em;'>WHITE</span></a>";
+  out += "<a href='black'><span style='display:inline-table; padding: 2% 6%; text-align:center; color:white; background-color:black; width:38%; height:3em;'>OFF / FADER OFF</span></a>";
   out += "</div>";
   out += "<span style='display:block; padding: 1% 6%; margin:auto; color: white; text-align:center;'>OcknigmaLabs-2016 # Sourcecode: <a href='#'>https://github.com/darthm0e/esp8266-witty</a></span>";
   out += "</body>";
@@ -75,6 +81,7 @@ void setup ( void ) {
 
     Serial.begin ( 115200 );
     WiFi.begin ( ssid, password );
+    WiFi.config(ip, gateway, subnet);
     Serial.println ( "" );
     // Wait for connection
     while ( WiFi.status() != WL_CONNECTED ) {
@@ -95,8 +102,9 @@ void setup ( void ) {
   server.on ( "/green", []() {colorWipe(strip.Color(0, 255, 0), 20); strip.show(); rainbow = false; theater = false; loops = false; handleRoot();} );
   server.on ( "/blue", []() {colorWipe(strip.Color(0, 0, 255), 20); strip.show(); rainbow = false; theater = false; loops = false; handleRoot();} );
   server.on ( "/pink", []() {colorWipe(strip.Color(200, 0, 200), 20); strip.show(); rainbow = false; theater = false; loops = false; handleRoot();} );
-  server.on ( "/black", []() {colorWipe(strip.Color(0, 0, 0), 10); strip.setBrightness(255); strip.show(); rainbow = false; theater = false; faders = false; loops = false; handleRoot();} );
+  server.on ( "/emerald", []() {colorWipe(strip.Color(0, 200, 200), 20); strip.show(); rainbow = false; theater = false; loops = false; handleRoot();} );
   server.on ( "/yellow", []() {colorWipe(strip.Color(200, 200, 0), 20); theater = false; loops = false; rainbow = false; strip.show(); handleRoot();} );
+  server.on ( "/black", []() {colorWipe(strip.Color(0, 0, 0), 10); strip.setBrightness(255); strip.show(); delay(100); handleRoot(); colorWipe(strip.Color(0, 0, 0), 10); rainbow = false; theater = false; faders = false; loops = false; strip.show(); handleRoot();} );
   server.on ( "/rainbow", []() {rainbow = true; theater = false; loops = false; handleRoot(); rainbowCycle(200); handleRoot();} );
   server.on ( "/chaser", []() {loops = false; theater = true; rainbow = false; faders = false; handleRoot(); theaterChaseRainbow(1000); handleRoot();} );
   server.on ( "/looper", []() {theater = false; loops = true; rainbow = false; faders = false; handleRoot(); looper(6000); handleRoot();} );
@@ -124,18 +132,19 @@ void fader(uint8_t wait) {
   strip.setBrightness(10);
   strip.show();
   
-  for(uint16_t i=10; i<255; i=i+10) {             
-      strip.show();
+  for(uint16_t i=10; i<255; i=i+15) {     
       delay(wait);      
       strip.setBrightness(i);
+      strip.show();      
   }
+  delay(200);
   for(uint16_t i=255; i>0; i=i-15) {             
-      strip.show();
       delay(wait);      
       strip.setBrightness(i);
+      strip.show();      
   }
   server.handleClient();
-  delay(wait*2);
+  delay(200);
   }  
 }
 
